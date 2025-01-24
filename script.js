452687e601cccba1c689674e52cfcc9777378d45
flowers4rish <rcdauz22@src.pshs.edu.ph>
2025-01-24 18:12:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const PI = 3.14;
 let circleRadius = 6.378e6;
-let circleCircumference = 2 * PI * r;
-let circleSurfaceArea = 4 * PI * (r ** 2);
-let circleVolume = (4/3) * PI * (r ** 3);
+let circleCircumference = 2 * PI * circleRadius;
+let circleSurfaceArea = 4 * PI * (circleRadius ** 2);
+let circleVolume = (4/3) * PI * (circleRadius ** 3);
 
 document.write('The radius of the sphere is ' + circleRadius + '.' + '<br>');
 document.write('The circumference is ' + circleCircumference + '.' + '<br>');
 document.write('The surface area is ' + circleSurfaceArea  + '.' + '<br>');
 document.write('The volume is ' + circleVolume + '.' + '<br>');
+
 
